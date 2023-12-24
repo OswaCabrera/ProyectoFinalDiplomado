fe075ea901db2bebf6af3dae4e541816d180399d
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-12-24 17:21:22 -0600

    Inicio de proyecto Final

---
+++ caja-ahorro/src/main/java/unam/diplomado/cajaahorro/CajaAhorroApplication.java
@@ -0,0 +1,13 @@
+package unam.diplomado.cajaahorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajaAhorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajaAhorroApplication.class, args);
+	}
+
+}
